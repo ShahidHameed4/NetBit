85eeb672927a0cc13a8073addfc037e6db43863f
shhdh <90132007+ShahidHameed4@users.noreply.github.com>
2023-05-19 15:11:03 +0500

    tailwind integration

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header >
+        
+        <p className="text-green-600 border-4 border-green-400">
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
